9eebbadb7e52d1211b7f7f2d0743a132439b654a
Anésio Neto <anesios98@gmail.com>
2023-09-29 21:33:51 -0300

    First commit: added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <img src="../src/img/React-icon.png" />
+                <h3>ReactFacts</h3>
+                <h4>React Course - Project 1</h4>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
